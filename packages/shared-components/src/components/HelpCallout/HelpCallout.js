cc3fb7bea939b428b69556a1d2e607711a724468
Denys Yefremov <denys.yefremov@gmail.com>
2018-09-26 18:19:47 +0300

    Removed unused import

--- packages/shared-components/src/components/HelpCallout/HelpCallout.js
+++ packages/shared-components/src/components/HelpCallout/HelpCallout.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Manager, Reference, Popper } from 'react-popper';
-import P from 'components/P';
 import HelpIcon from './styles/HelpIcon';
 import Container from './styles/HelpCalloutContainer';
 
